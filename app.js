fa9569353fa2385f1005e7f7106f113b3726fc28
saitejaswini2525 <saitejaswiniyarapathineni1234@gmail.com>
2022-11-02 21:12:39 -0500

    gridbuild was created

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogRouter = require('./routes/dog');
-
+var gridbuildRouter = require('./routes/gridbuild');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dog', dogRouter);
+app.use('/gridbuild', gridbuildRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
